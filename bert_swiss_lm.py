e212acfd750800c5b030119fe536df909002909d
Raphael Schilling <raphaelschilling@web.de>
2023-04-24 11:48:50 +0200

    Add precalculated embeddings

--- bert_swiss_lm.py
+++ bert_swiss_lm.py
@@ -16,17 +16,17 @@ class BertSwissLm:
         if set_default_language_to_de_CH:
             self.model.set_default_language("de_CH")
 
-    def _get_sentence_embedding(self, text) -> tuple[np.ndarray, np.ndarray]:
+    def _get_sentence_embedding(self, text) -> np.ndarray:
         inputs = self.tokenizer(text, return_tensors="pt")
         with torch.no_grad():
             outputs = self.model(**inputs)
             last_hidden_state = outputs.last_hidden_state[0, 0, :]
-            pooler_output = outputs.pooler_output[0, :]
-        return last_hidden_state.numpy(), pooler_output.numpy()
+            #pooler_output = outputs.pooler_output[0, :]
+        return last_hidden_state.numpy()
 
     def add_sentence_embedding_to_df(self, df: pd.DataFrame) -> pd.DataFrame:
         df = df.copy()
-        df[["sentence_embedding_first_state", "sentence_embedding_pooler"]] = df["text"].apply(
+        df["sentence_embedding_first_state"] = df["text"].apply(
             self._get_sentence_embedding).apply(pd.Series)
         return df
 
